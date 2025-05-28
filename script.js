14c74384ffb6f5d90fd12136f9bef8dba16d3bbf
Joakimglodedata <joakim.glodedata@gmail.com>
2025-05-28 12:07:44 +0200

    push

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+const submitButton = document.getElementById("submitButton");
+console.log(submitButton);
+
+submitButton.addEventListener("click", function () {
+    console.log("Button is clicked?");
+    event.preventDefault();
+
+    const inputNumber = document.getElementById("inputNumber").value
+    console.log(inputNumber);
+    console.log(inputNumber + inputNumber)
+
+    const selectOptions = document.getElementById("selectOptions").value
+    console.log(selectOptions)
+
+    if (selectOptions === "pickUp") {
+        // do this
+    } else if (selectOptions === "deliver") {
+        // do that
+    } else {
+        // Do something else
+    }
+})
